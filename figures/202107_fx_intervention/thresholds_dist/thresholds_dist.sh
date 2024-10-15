a9a91ca757c8353bc8ff68c6dc49941998a02600
Rory Mullen <rorymullen@Rorys-MacBook-Pro.local>
2024-10-15 13:07:25 +0100

    Updated SVG and made text adjustments

--- figures/202107_fx_intervention/thresholds_dist/thresholds_dist.sh
+++ figures/202107_fx_intervention/thresholds_dist/thresholds_dist.sh
@@ -8,10 +8,4 @@ latex --shell-escape thresholds_dist.tex
 dvisvgm thresholds_dist.dvi -o thresholds_dist.svg
 
 # 3. Optimize the SVG file using svgo (optional)
-svgo thresholds_dist.svg
-
-# 4. Modify the SVG to preserve text size (optional)
-# This will add 'text' to preserve text scaling
-xmlstarlet ed -L \
-  -u "//text" -v "preserve" \
-  thresholds_dist.svg
+svgo thresholds_dist.svg 
